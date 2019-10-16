162fca00a1430da71e2b4bfe3f4de00e18eb68f8
jastring <tripp.stringfield@gmail.com>
2019-10-16 12:23:35 -0400

    setup friends.js and home.html

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require("express");
+var path = require("path")
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static(path.join(__dirname, './app/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
