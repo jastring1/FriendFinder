6efc7a7429b283994ebed0893210af18f7cb454e
jastring <tripp.stringfield@gmail.com>
2019-10-16 11:53:06 -0400

    Setup server.js and htmlRoutes

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+
+var friendArray = [
+    {
+        name: "Tripp",
+        image: "image url",
+        scores: [1,2,3,4,5,5,4,3,2,1]
+    },
+    {
+        name: "Wes",
+        image: "image url",
+        scores: [2,3,1,4,5,4,2,3,1,5]
+    },
+    {
+        name: "Haley",
+        image: "image url",
+        scores: [5,4,3,2,1,1,2,3,4,5]
+    },
+    {
+        name: "Margaret",
+        image: "image url",
+        scores: [3,3,3,2,4,5,5,3,2,1]
+    },
+
+]
+
+module.exports = friendArray
