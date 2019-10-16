8b1336416b7e0479c3794b5cf906c6d4a2dda59f
jastring <tripp.stringfield@gmail.com>
2019-10-16 15:32:37 -0400

    added logic and modal for determing best match and returning that information

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,9 +12,27 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
 
     var friend = req.body
+    // changing the asnwers from the survey into integers to be able to calculate friend scores
     for(var i = 0; i < friend.scores.length; i++) {
         friend.scores[i] = parseInt(friend.scores[i]);
       }
+    //setting up variables to determines the best match
+    var lowestDifference = 100
+    var matchIndex = 0
+      //nested loop that calculates the total difference from the user to each user saved in the database
+    for (var i=0;i<friendArray.length;i++){
+        var totalDifference = 0
+        for (var j=0;j<10;j++){
+            totalDifference += Math.abs(friend.scores[j] - friendArray[i].scores[j])
+        }//conditional to determine the index of the best match to the user
+        if(totalDifference < lowestDifference){
+            matchIndex = i
+            lowestDifference = totalDifference
+        }
+    }
+    //adding a new user to the friendArray after determining the match, this happens so that the user doesnt match with themselves
     friendArray.push(friend)
+    //returning the best match
+    res.json(friendArray[matchIndex])
   });
 }
