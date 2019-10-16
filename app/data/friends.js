903ac63f6236c1a022a8d66d8922976b2c237830
jastring <tripp.stringfield@gmail.com>
2019-10-16 18:22:20 -0400

    fixing images

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,22 +3,22 @@ var path = require("path")
 var friendArray = [
     {
         name: "Tripp",
-        image: path.join(__dirname,"'./app/images/tripp.jpg'"),
+        image: "https://miro.medium.com/max/1742/1*XLkyIpaeYOL52sS2QF7DLA.png",
         scores: [1,2,3,4,5,5,4,3,2,1]
     },
     {
         name: "Wes",
-        image: path.join(__dirname,"./app/images/ali.jpg"),
+        image: "https://www.pikpng.com/pngl/m/72-727346_spongebob-characters-hd-png-download.png",
         scores: [2,3,1,4,5,4,2,3,1,5]
     },
     {
         name: "Haley",
-        image: path.join(__dirname,"./app/images/haley.jpg"),
+        image: "https://miro.medium.com/max/1800/1*DCo5LIbgIWrFIOfl3UAMpA.png",
         scores: [5,4,3,2,1,1,2,3,4,5]
     },
     {
         name: "Margaret",
-        image: path.join(__dirname,"./app/images/margaret.jpg"),
+        image: "https://i.pinimg.com/originals/81/53/6d/81536d62e6158f4c11e9cb2dfed53e15.jpg",
         scores: [3,3,3,2,4,5,5,3,2,1]
     },
 
